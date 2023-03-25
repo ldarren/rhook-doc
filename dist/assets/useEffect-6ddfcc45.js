a0c39895333e9eb0ef6b0b0dd5b97ce064ce4d19
Darren Liew <ldarren@gmail.com>
2023-03-26 06:21:45 +0800

    added dist

---
+++ dist/assets/useEffect-6ddfcc45.js
@@ -0,0 +1,23 @@
+const n=`import React, { useState, useEffect } from 'react'
+
+function Example() {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount(count => count + 1)
+    }, 1000)
+
+    // Clean up the interval when the component unmounts
+    return () => clearInterval(interval)
+  }, [])
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+    </div>
+  )
+}
+
+<Example/>
+`;export{n as default};
---
+++ dist/assets/useState-97dc916e.js
@@ -0,0 +1,24 @@
+const n=`import React, { useState } from 'react'
+
+function Example() {
+  const [count, setCount] = useState(0)
+
+  function handleIncrement() {
+    setCount(old => old + 1)
+  }
+
+  function handleReset() {
+    setCount(0)
+  }
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleReset}>Reset</button>
+    </div>
+  )
+}
+
+<Example/>
+`;export{n as default};
